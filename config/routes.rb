cf01d0c20fb2f96f3cf57d9f57ff38dc8142a8f7
李智慧 <lizhihui@lizhihuis-MacBook-Pro.local>
2015-10-14 02:04:58 +0800

    add login and session

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,25 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   get 'users/new'
 
   root 'static_pages#home'
 
   # get 'static_pages/help'
-  get 'help' => 'static_pages#help'
-
+  get 'help'      => 'static_pages#help'
   # get 'static_pages/about'
-  get 'about' => 'static_pages#about'
-
+  get 'about'     => 'static_pages#about'
   # get 'static_pages/contact'
-  get 'contact' => 'static_pages#contact'
-
-  get 'signup' => 'users#new'
+  get 'contact'   => 'static_pages#contact'
+  get 'signup'    => 'users#new'
+  get 'login'     => 'sessions#new'
+  post 'login'    => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
