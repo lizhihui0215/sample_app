1a2e781fb2b7516657f837c1bd0f5aaa9bff27e8
李智慧 <lizhihui@lizhihuis-MacBook-Pro.local>
2015-10-16 04:26:03 +0800

    change production.rb host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,15 +80,15 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'http://52.11.151.111:9080/'
+  host = 'enigmatic-peak-6302.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['lizhihui0215'],
-    :password       => ENV['dsn4cgwy'],
-    :domain         => 'http://52.11.151.111/',
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
 end
